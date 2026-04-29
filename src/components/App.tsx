6d68b987d7b7cea7072838f7492e59ca3cdc1dcf
kikeda1102 <kikeda1102@gmail.com>
2026-04-29 14:13:34 +0900

    feat: Featuresコンポーネントの位置をHighlightsの後に移動しました

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,8 +13,8 @@ export default function App() {
       <Header />
       <main>
         <Hero />
-        <Features />
         <Highlights />
+        <Features />
         <Pricing />
         <BoothCta />
       </main>
